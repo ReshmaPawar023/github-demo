2d1e81382f76b1a9dfc6109a64e5653368921cc9
reshmapawar <reshma_pawar@persistent.com>
2021-03-22 16:35:08 +0530

    Function headers added

--- math1.py
+++ math1.py
@@ -1,9 +1,13 @@
+#add 
 def add(x,y):
     pass
+#subtract
 def subtract(x,y):
     pass
+#multiply
 def multiply(x,y):
     return x*y
+#divide
 def divide(x,y):
 	if y==0:
 		return DIVIDE_BY_0_ERROR
